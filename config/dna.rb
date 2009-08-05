660298734015815599bd3e0ec27ed87315eb3b67
Guoliang Cao <gcao@vonage.com>
2009-08-05 09:06:14 -0400

    Initial commit

---
+++ config/dna.rb
@@ -0,0 +1,83 @@
+require 'rubygems'
+require 'json'
+
+dna = {
+  :user => "gcao",
+  :mysql_root_pass => "REPLACE_ME!",
+  
+  :users =>  [
+    {
+      :username => "gcao",
+      :password => "REPLACE_ME!",
+      :authorized_keys => "ssh-rsa ACfSXuMTJvY6Ys6zdxRljhzBbh/XHU8= bob@bob.com",
+      :shell => "/bin/bash",
+      :gid => 1000,
+      :uid => 1000,
+      :sudo => true,
+      :custom_files  => []
+    },
+    
+    {
+      :username => "gocool_app",
+      :gid => 1101,
+      :uid => 1101,
+      :authorized_keys => ["ssh-rsa ACfSXuMTJvY6Ys6zdxRljhzBbh/XHU8= bob@bob.com", "ssh-rsa ACfSXuMTJvY6Ys6zdxRljhzBbh/XHU8= cindy@bob.com"],
+      :shell => "/bin/bash",
+    }
+  ],
+  
+  :packages => [
+  ],
+  
+  :applications => [
+    {
+      :name => "gocool",
+      :server_names => "_",
+      :ports => [4000, 4001],
+      :user => "gocool_app",
+      :group => "gocool_app"
+      #:custom_nginx_conf => File.read(File.dirname(__FILE__), "../config/custom/some-custom-nginx.conf")
+    }
+  ],
+  
+  :gems => [
+    "rake", 
+    {:name => "mysql", :version => "2.7"}
+  ],
+  
+  # :ebs_volumes => [
+  #   {:device => "sdq1", :path => "/data"},
+  #   {:device => "sdq2", :path => "/db"}
+  # ],
+  
+  :cronjobs => [
+    {:name => "a_dumb_task",
+     :minute => 30,
+     :hour => nil,
+     :day => nil,
+     :month => nil,
+     :weekday => nil,
+     :user => "mr_app",
+     :command => "date >> /data/look_cron_works.txt"
+    }
+  ],
+  
+  :recipes => [
+    "apache2",
+    "packages",
+    "users",
+    "sudo",
+    "openssh",
+    # "ec2-ebs",
+    "mysql",
+    "git",
+    "logrotate",
+    # "nginx",
+    "memcached",
+    "cron",
+    "gems",
+    "rack_apps"
+  ]
+}
+
+open(File.dirname(__FILE__) + "/dna.json", "w").write(dna.to_json)
